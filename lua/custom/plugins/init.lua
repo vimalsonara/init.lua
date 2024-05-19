fc865f2d6c691366db0dfaebc22647e9c638bff5
Vimal Sonara <vvsonara95@gmail.com>
2024-05-19 15:00:09 +0530

    add codeium to custom plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  --codeium plugin
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+  },
+  --wakatime plugin
+  { 'wakatime/vim-wakatime', lazy = false },
+}
