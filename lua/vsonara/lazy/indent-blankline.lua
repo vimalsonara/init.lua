17012d65b802253d86ff3cf71ed9944bbe656530
Vimal Sonara <vvsonara95@gmail.com>
2024-07-09 12:57:46 +0530

    indent rainbow color

--- lua/vsonara/lazy/indent-blankline.lua
+++ lua/vsonara/lazy/indent-blankline.lua
@@ -5,6 +5,15 @@ return {
       indent = {
         char = "│",
         tab_char = "│",
+        highlight = {
+          "RainbowRed",
+          "RainbowYellow",
+          "RainbowBlue",
+          "RainbowOrange",
+          "RainbowGreen",
+          "RainbowViolet",
+          "RainbowCyan",
+        }
       },
       scope = { show_start = false, show_end = false },
       exclude = {
@@ -23,6 +32,21 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      local hooks = require("ibl.hooks")
+
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
+      require("ibl").setup(opts)
+    end,
     main = "ibl",
   }
 }
