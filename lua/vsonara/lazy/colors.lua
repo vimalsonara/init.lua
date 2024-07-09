0b75685240bc24d5dcffb9c4b63c800e081d8138
Vimal Sonara <vvsonara95@gmail.com>
2024-07-09 07:08:01 +0530

    add harpoon

--- lua/vsonara/lazy/colors.lua
+++ lua/vsonara/lazy/colors.lua
@@ -3,8 +3,8 @@ function ColorMyPencils(color)
   vim.cmd.colorscheme(color)
 
   -- this will make background transparent
-  --  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
 return {
