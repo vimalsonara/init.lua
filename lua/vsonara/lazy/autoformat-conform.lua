4a2f89f503e5f2faddd522450d8ff284e99401af
Vimal Sonara <vvsonara95@gmail.com>
2024-07-09 15:51:24 +0530

    conform prettier config

--- lua/vsonara/lazy/autoformat-conform.lua
+++ lua/vsonara/lazy/autoformat-conform.lua
@@ -26,12 +26,15 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
       },
     },
   },
